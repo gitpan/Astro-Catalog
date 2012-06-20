650b82bf772ccdbf9361540166575a90e3f727cf
Tim Jenness <t.jenness@jach.hawaii.edu>
2012-06-20 21:33:39 +0000

    Import of TJENNESS/Astro-Catalog-4.22 from CPAN.
    gitpan-cpan-distribution: Astro-Catalog
    gitpan-cpan-version:      4.22
    gitpan-cpan-path:         TJENNESS/Astro-Catalog-4.22.tar.gz
    gitpan-cpan-author:       TJENNESS
    gitpan-cpan-maturity:     released

--- lib/Astro/Catalog/IO/ASCII.pm
+++ lib/Astro/Catalog/IO/ASCII.pm
@@ -24,7 +24,7 @@ use strict;
 
 use vars qw/ $VERSION $DEBUG /;
 
-$VERSION = '0.02';
+$VERSION = '0.03';
 $DEBUG = 0;
 
 =head1 METHODS
@@ -73,7 +73,7 @@ sub read_catalog {
       # must be a scalar
       @lines = split /\n/, $args{data};
     } else {
-      if (ref($args{data}) eq 'GLOB') {
+      if (ref($args{data}) eq 'GLOB' || UNIVERSAL::isa($args{data},"IO::Handle") ) {
         # A file handle
         local $/ = "\n";
         # For some reason <$args{data}> does not do the right thing
